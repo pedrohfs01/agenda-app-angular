ddafe0d391a50515cdce921ac972b42f3ec50940
Pedro Henrique Fernandes Santos <pedexg@gmail.com>
2021-01-04 14:07:14 -0300

    Melhorar layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,10 @@ import { NgModule } from '@angular/core';
 import { MatButtonModule } from "@angular/material/button"
 import { MatToolbarModule } from "@angular/material/toolbar"
 import { MatInputModule } from "@angular/material/input"
+import { MatTabsModule} from "@angular/material/tabs"
+import { MatTableModule} from "@angular/material/table"
+import { MatIconModule} from "@angular/material/icon"
+import { MatCardModule } from "@angular/material/card"
 
 import { HttpClientModule} from "@angular/common/http"
 
@@ -27,6 +31,10 @@ import { ReactiveFormsModule } from '@angular/forms';
     MatButtonModule,
     MatToolbarModule,
     MatInputModule,
+    MatTabsModule,
+    MatTableModule,
+    MatIconModule,
+    MatCardModule,
 
     BrowserAnimationsModule,
     HttpClientModule
